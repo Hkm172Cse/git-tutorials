2d5f0e2756b1c2fc45b3c18c697118fc2c5ab066
Hkm172Cse <azizulhakim000111@gmail.com>
2021-10-14 02:23:46 +0600

    add new Method add in hellow.java file

--- hellow.java
+++ hellow.java
@@ -7,4 +7,12 @@ public class hellow{
     public static void display(String name){
         System.out.println("My name is "+ name);
     }
+
+    public static void sum(int a, int b){
+        int sumetion = a + b;
+    }
+    public static void sum(int a, int b, int c){
+        int three= a + b + c;
+    }
+
 }
