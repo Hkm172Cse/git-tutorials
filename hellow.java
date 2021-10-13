d447942e9bdd95efba40ded3a1634d4d78b1ef68
Hkm172Cse <azizulhakim000111@gmail.com>
2021-10-14 02:06:54 +0600

    added a display Method in hellow.java file

--- hellow.java
+++ hellow.java
@@ -1,5 +1,10 @@
 public class hellow{
     public  static void main(String[] args){
         System.out.println("Visual Studio code is awasome");
+        display("Azizul Hakim");
+    }
+
+    public static void display(String name){
+        System.out.println("My name is "+ name);
     }
 }
